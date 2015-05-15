3eda5457bd3bdf223a5d222dca870a659d159005
Novica <novicamiketic@gmail.com>
2015-05-15 13:59:16 +0200

    promjene

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
 		<script type="text/javascript" src="js/responsiveslides.js"></script>
 		<link rel="stylesheet" href="css/style-mobile.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
 		<script>
 			
 			$(function(){
